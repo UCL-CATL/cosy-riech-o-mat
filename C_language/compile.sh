ce86102652b26f6f281a83735e388149da70fdb6
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2018-03-05 12:11:30 +0100

    Test in C (it works!)
    Now I need to figure out why it doesn't work in Matlab.

---
+++ C_language/compile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+~/.vim/bin/cc_args.py gcc -W -Wall -o test-open-valve -g test-open-valve.c
---
+++ C_language/test-open-valve.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+
+#define DEVICE_FILE "/dev/ttyS0"
+
+/* Returns the file descriptor on success or -1 on error. */
+int
+open_serial_port (void)
+{
+	int fd;
+
+	/* Can try also without O_NDELAY. */
+	fd = open (DEVICE_FILE, O_RDWR | O_NOCTTY | O_NDELAY);
+	if (fd == -1)
+	{
+		perror ("Unable to open " DEVICE_FILE " - ");
+	}
+	else
+	{
+		fcntl (fd, F_SETFL, 0);
+	}
+
+	return fd;
+}
+
+int
+main (void)
+{
+	int fd;
+	ssize_t n_bytes_written;
+	int ret = EXIT_SUCCESS;
+
+	fd = open_serial_port ();
+	if (fd == -1)
+	{
+		return EXIT_FAILURE;
+	}
+
+
+	n_bytes_written = write (fd, "\x55\x56\x00\x00\x00\x01\x01\xad", 8);
+	if (n_bytes_written < 0)
+	{
+		perror ("write() failed - ");
+		ret = EXIT_FAILURE;
+	}
+	else if (n_bytes_written != 8)
+	{
+		fprintf (stderr,
+			 "write() failed: %ld bytes written, should be 8.\n",
+			 n_bytes_written);
+		ret = EXIT_FAILURE;
+	}
+
+	if (close (fd) == -1)
+	{
+		perror ("close() failed - ");
+		ret = EXIT_FAILURE;
+	}
+
+	return ret;
+}

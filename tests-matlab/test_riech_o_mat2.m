98e6657aca864207c78bf5a3c278c7b54c7031f3
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2018-05-14 15:30:46 +0200

    test matlab: add comment about running matlab as root

--- tests-matlab/test_riech_o_mat2.m
+++ tests-matlab/test_riech_o_mat2.m
@@ -1,3 +1,6 @@
+% On Linux it's possible that this script needs to be run as root, otherwise it
+% cannot create a lock file (even if the user is in the dialout group).
+
 % Must be: baud rate 9600, 8 data bits, 1 stop bit, no parity (all default
 % values).
 % Reduce timeout to 1 second. In riech-o-mat-backend-iowarrior.c (so in

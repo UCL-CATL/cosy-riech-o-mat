6a74f3e34534313e11210142aee02bdb24bacd93
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2018-05-17 16:01:13 +0200

    matlab: add TODO comment

--- matlab/cosy_riech_o_mat.m
+++ matlab/cosy_riech_o_mat.m
@@ -1,2 +1,4 @@
-function cosy_riech_o_mat(valves_states)
+% TODO implement it like C-backend/riech-o-mat-backend-serial.c.
+% See tests-matlab/test_riech_o_mat2.m for a prototype.
+function cosy_riech_o_mat(valves_positions)
 end

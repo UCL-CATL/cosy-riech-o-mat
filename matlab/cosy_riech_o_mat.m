f649eecf0318871089b3db4fd08d79270b5bb93a
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2018-06-12 15:57:05 +0200

    Rename directory C-backend/ -> src/
    src/ will contain the main source code.

--- matlab/cosy_riech_o_mat.m
+++ matlab/cosy_riech_o_mat.m
@@ -1,4 +1,4 @@
-% TODO implement it like C-backend/riech-o-mat-backend-serial.c.
+% TODO implement it like riech-o-mat-backend-serial.c.
 % See tests-matlab/test_riech_o_mat2.m for a prototype.
 function cosy_riech_o_mat(valves_positions)
 end

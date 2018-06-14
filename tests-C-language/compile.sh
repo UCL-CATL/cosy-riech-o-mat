4da43fd72be439c1b1bb74e18ce6ce665bc72c5f
Sébastien Wilmet <sebastien.wilmet@uclouvain.be>
2018-06-14 11:30:11 +0200

    build: use Meson

--- tests-C-language/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# For me, to have clang completion in Vim:
-~/.vim/bin/cc_args.py gcc -W -Wall -o test-open-valve -g test-open-valve.c
-~/.vim/bin/cc_args.py gcc -W -Wall -o test-parallel-port -g test-parallel-port.c
-
-#gcc -W -Wall -o test-open-valve -g test-open-valve.c
